02ab057b05ba384b1354d488195df69bad38dbcc
Adarsh Pachori <adarshpachori21@gmail.com>
2022-07-14 17:38:54 -0700

    adding compliments to button.

--- js/index.js
+++ js/index.js
@@ -111,3 +111,48 @@ var love = setInterval(function () {
 //start of carousel
 $(".carousel").carousel();
 //end of carousel
+/* */
+var arrayOfCompliments = [
+  "1. Your eyes are absolutely beautiful.",
+  "2. You have the prettiest smile.",
+  "3. Seeing you laugh makes me happier than anything else in the world.",
+  "4. You're super funny. I love all the vlogs you post on your sc story (the chocolate-ratings in London, das quality entertainment!)",
+  "5. I'm your biggest fan :-)",
+  "6. That ass 👀 shIEEEEE",
+  "7. You playing with your putty is pretty cute.",
+  "8. You are so caring for me and all your friends. I think I'm speaking for everyone when I say, we are all very grateful to have you so close to us.",
+  "9. I love how passionate you are about what you believe in. ",
+  "10. Your hard work ethic is truly inspiring. There is no doubt in my mind that you are going to achieve all your goals and more.",
+  "11. You are super understanding and patient with me when I mess up.",
+  "12. I love how much you love Bolt hehe it's adorable",
+  "13. You always want the best for me and I am very grateful for that.",
+  "14. You give the best cuddles.",
+  "15. You never cease to awe me with them fire fits.",
+  "16. I feel like I can tell you anything and you'll just be an awesome listener.",
+  "17. I love that you take charge in our trips by making the itinerary in visiting new places and having new experiences.",
+  "18. I love how confident you are in yourself. It makes you shine!",
+  "19. I love that you just spontaneously nap, and have trouble waking up from naps. It's pretty funny HAHA.",
+  "20. You push me to be the version of myself.",
+  "21. I can be myself around you. ",
+  "22. You are the definition of a strong, indepedent woman and I love it.",
+];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+var complimentPTag = document.getElementById("complimentPTag");
+var complimentButton = document.getElementById("complimentButton");
+complimentButton.onclick = function (event) {
+  if (arrayOfCompliments.length >= 1) {
+    console.log("hi");
+    var rand_int = getRandomInt(arrayOfCompliments.length);
+    complimentPTag.innerHTML = arrayOfCompliments[rand_int];
+    $("#complimentPTag").fadeIn();
+    arrayOfCompliments.splice(rand_int, 1);
+  } else {
+    complimentPTag.innerHTML =
+      "You've went through all the compliments I've written here but there's hundreds of more amazing things about you that I plan to add to this list soon.";
+  }
+};
+
+/**/
